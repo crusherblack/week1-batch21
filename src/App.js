5d5b89ab4bd21cdd98efe25ddefa2357a3514b44
fadhil <fadhildarma13@gmail.com>
2021-03-10 10:13:46 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import { useState } from "react";
-import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// CSS
-// inline styling
-// Memindahkan inline styling ke sebuah variable
-
 const App = () => {
-  const [isRed, setIsRed] = useState(true);
-
-  const changeBackround = () => {
-    setIsRed((prev) => {
-      return !prev;
-    });
-  };
-
   return (
-    <div className={`app-cont ${isRed ? "bg-primary" : "bg-secondary"}`}>
-      <header className="app-header"></header>
-      <div style={styles.contentCont}>
-        <button onClick={changeBackround}>Change Backround</button>
-        <pre className="text-white text-medium">
-          {JSON.stringify(isRed, 2, null)}
-        </pre>
-      </div>
+    <div>
+      <button className="btn btn-primary">DARI BOOTSTRAP</button>
     </div>
   );
-};
-
-const styles = {
-  contentCont: {
-    display: "flex",
-    minHeight: "80vh",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    flexDirection: "column",
-    margin: "30px 20px 50px",
-  },
 };
 
 export default App;
